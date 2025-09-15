eae74c9f578877cab2411d5a62a2c97b80ef2646
Matthias Nyman <natthias@proton.me>
2025-09-15 20:54:29 +0200

    user/libastal-wireplumber: new package

---
+++ user/libastal-wireplumber/template.py
@@ -0,0 +1,25 @@
+pkgname = "libastal-wireplumber"
+# sync with libastal4
+pkgver = "0_git20253004"
+_commit = "67ddc83e0bdbda6de7f6f15e4fbc5d6b9d2d1b18"
+pkgrel = 0
+build_wrksrc = "lib/wireplumber"
+build_style = "meson"
+configure_args = ["-Ddefault_library=shared"]
+hostmakedepends = [
+    "gobject-introspection",
+    "meson",
+    "pkgconf",
+    "vala",
+]
+makedepends = ["wireplumber-devel"]
+pkgdesc = "WirePlumber library for libastal"
+license = "LGPL-2.1-or-later"
+url = "https://github.com/Aylur/astal"
+source = f"https://github.com/Aylur/astal/archive/{_commit}.tar.gz"
+sha256 = "fe9ac3e126271de1d871ca0d5992be952835df2d89765181df1180e31cbf0391"
+
+
+@subpackage("libastal-wireplumber-devel")
+def _(self):
+    return self.default_devel()
