cbfe7ac919341c6ced460dd981f2b43474c6f7eb
Matthias Nyman <natthias@proton.me>
2025-09-15 20:49:36 +0200

    user/gtklock: new package

---
+++ user/gtklock/template.py
@@ -0,0 +1,29 @@
+pkgname = "gtklock"
+pkgver = "4.0.0"
+pkgrel = 0
+build_style = "meson"
+hostmakedepends = [
+    "meson",
+    "pkgconf",
+    "scdoc",
+]
+makedepends = [
+    "glib-devel",
+    "gtk+3-devel",
+    "gtk-layer-shell-devel",
+    "gtk-session-lock-devel",
+    "linux-pam-devel",
+    "musl-devel",
+    "wayland-devel",
+]
+pkgdesc = "GTK-based lockscreen for Wayland"
+license = "GPL-3.0-or-later"
+url = "https://github.com/jovanlanik/gtklock"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "db20bf27bd5dd01901ea1753c89c170777dd7cf8fca19130cf90f5f4e3fb9633"
+# no tests
+options = ["!check"]
+
+
+def post_install(self):
+    self.rename("etc/pam.d", "usr/lib/pam.d", relative=False)
