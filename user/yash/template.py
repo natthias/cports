cf1ced65cae1af1b3f2fd547b9194ace8791314e
q66 <q66@chimera-linux.org>
2026-05-03 16:00:37 +0200

    user/yash: disable check on loongarch

--- user/yash/template.py
+++ user/yash/template.py
@@ -22,6 +22,12 @@ license = "GPL-2.0-only"
 url = "https://github.com/magicant/yash"
 source = f"{url}/releases/download/{pkgver}/yash-{pkgver}.tar.xz"
 sha256 = "a214966f4ff8b293aa5521a4d3ef6e87d707579eee616aa2f8218edaa920d447"
+# check may be off
+options = []
+
+if self.profile().arch in ["loongarch64"]:
+    # some permissions tests fail, same behavior observed elsewhere
+    options += ["!check"]
 
 
 def post_install(self):
